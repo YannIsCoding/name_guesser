312ef3f692bccaafe58f658ebcfb00dda73e28e5
Yann <yann.parent4@gmail.com>
2024-02-21 12:12:33 +0100

    Add test for non-ASCII charactere

--- test/integration/app_test.rb
+++ test/integration/app_test.rb
@@ -7,20 +7,18 @@ require 'rack/test'
 class AppTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
-  def app
-    Sinatra::Application
-  end
+  def app = Sinatra::Application
 
-  test 'with a known name and standard ascii charactere' do
-    get '/?name=Martin'
+  test 'with a known name and standard ascii charactere, return a guess' do
+    get '/', :name => 'Martin'
 
     assert last_response.ok?
-    expectation = { guessed_country: ['DE'], requested_name: 'Martin', time_processed: [] }.to_json
-    assert_equal expectation, last_response.body
   end
 
-  # def test_it_says_hello_to_a_person
-  #   get '/', :name => 'Simon'
-  #   assert last_response.body.include?('Simon')
-  # end
+  test 'with a known name and non-standard ascii charactere' do
+    get '/', :name => 'Müller'
+
+    expectation = { guessed_country: ['DE'], requested_name: 'Müller', time_processed: [] }.to_json
+    assert_equal expectation, last_response.body
+  end
 end
