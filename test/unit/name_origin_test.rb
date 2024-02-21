05e61b3b808c2d35aa2e55bf24d2fa7b983e10c7
Yann <yann.parent4@gmail.com>
2024-02-21 12:36:24 +0100

    Fix unit test styling

--- test/unit/name_origin_test.rb
+++ test/unit/name_origin_test.rb
@@ -1,9 +1,9 @@
-require "minitest/autorun"
+require 'minitest/autorun'
 require 'app/name_origin'
 
 describe NameOrigin do
-  describe "#guess" do
-    describe "with a German name" do
+  describe '#guess' do
+    describe 'with a German name' do
       it "return ['DE']" do
         name_origin = NameOrigin.new('Müller')
 
@@ -11,8 +11,8 @@ describe NameOrigin do
       end
     end
 
-    describe "with a name belonging to serveral countries" do
-      it "return the array of possibility" do
+    describe 'with a name belonging to serveral countries' do
+      it 'return the array of possibility' do
         name_origin = NameOrigin.new('Lange')
 
         _(name_origin.guess).must_equal(['DE', 'NL'])
@@ -20,19 +20,27 @@ describe NameOrigin do
     end
   end
 
-  describe "with a unknown name" do
-    it "return an empty array" do
+  describe 'with a unknown name' do
+    it 'return an empty array' do
       name_origin = NameOrigin.new('Mysteryname')
 
       _(name_origin.guess).must_equal([])
     end
   end
 
-  describe "with special character" do
+  describe 'with special character' do
     it "return an empty array" do
       name_origin = NameOrigin.new('My$teryn!@e1234')
 
       _(name_origin.guess).must_equal([])
+    end
+  end
+
+  describe 'with special capitalization' do
+    it 'return a match if any' do
+      name_origin = NameOrigin.new('marTIn')
+
+      _(name_origin.guess).must_equal(['DE'])
     end
   end
 end
