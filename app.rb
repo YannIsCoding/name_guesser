1d741693511dcc2c708c0b88b4621fcec0f72e1d
Yann <yann.parent4@gmail.com>
2024-02-23 11:25:48 +0100

    Refactor minor styling element

--- app.rb
+++ app.rb
@@ -3,12 +3,12 @@ require_relative 'app/name_origin'
 
 get '/' do
   start_time = Time.now
-  name = params['name']
+  requested_name = params['name']
 
-  if name.nil?
+  if requested_name.nil?
     return { guessed_country: [], requested_name: nil, time_processed: [(Time.now - start_time)] }.to_json
   end
 
-  guessed_country = NameOrigin.new(name).guess
-  { guessed_country:, requested_name: name, time_processed: [(Time.now - start_time)] }.to_json
+  guessed_country = NameOrigin.new(requested_name).guess
+  { guessed_country:, requested_name:, time_processed: [(Time.now - start_time)] }.to_json
 end
